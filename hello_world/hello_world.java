38dd9beb17b9f2f305131f9bdd3670a8a3855ea6
javierP17 <j.palaciosmartinez.jp@gmail.com>
2016-10-03 18:39:05 +0200

    v0.import hello world

---
+++ hello_world/hello_world.java
@@ -0,0 +1,9 @@
+//Declaració de la classe
+public class hello_world {
+
+	//Definició del mètode main
+	public static void main(String[] args) {
+		System.out.println("Hello World from Java!");
+	
+	}
+}

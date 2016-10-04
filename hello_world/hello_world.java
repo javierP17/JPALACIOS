d44ecc414d52d13097483fe73a2b57d5a865de5e
javierP17 <j.palaciosmartinez.jp@gmail.com>
2016-10-04 20:39:48 +0200

    modificacio

--- hello_world/hello_world.java
+++ hello_world/hello_world.java
@@ -3,7 +3,6 @@ public class hello_world {
 
 	//Definició del mètode main
 	public static void main(String[] args) {
-		System.out.println("Hello World from Java!");
-	
+		System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
 	}
 }
